e98cb74a9988f76a7201fc260689bacc84e7a0d9
Wenhao Zhao <zhaowh1987@gmail.com>
2023-02-06 22:21:27 +0800

    update project 04

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -10,3 +10,27 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@2
+M = 0
+
+(LOOP)
+    @1
+    M = M - 1
+    D = M
+    @END
+    D;JLT
+
+    @0
+    D = M
+    @2
+    M = M + D
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
+
+
